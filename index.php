f5444b224311008078918a5b410d06e9378d8ad2
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-18 21:32:07 +0200

    show hotels details on page

--- index.php
+++ index.php
@@ -41,3 +41,26 @@
     ];
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous'/>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Elenco Hotel</h1>
+
+    <?php foreach ($hotels as $hotel) : ?>
+        <div>
+            <h2><?php echo $hotel['name']; ?></h2>
+            <p><?php echo $hotel['description']; ?></p>
+            <p>Parcheggio: <?php echo $hotel['parking'] ? 'si' : 'no'; ?></p>
+            <p>Voto: <?php echo $hotel['vote']; ?></p>
+            <p>Distanza dal centro: <?php echo $hotel['distance_to_center']; ?> km</p>
+        </div>
+        <hr>
+    <?php endforeach; ?>
+</body>
+</html>
